f7f22e61716a5f56244cf0db7ea355ae3205f310
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-04-22 21:11:46 +0100

    styled 404 page and added random message generator for 404 page

---
+++ js/404-random-message.js
@@ -0,0 +1,16 @@
+const messageElem = document.querySelector('#randomisedMessage'),
+messages = [
+  'Needs. More. Dakka.',
+  'Twas a silly place anyway, best not go there.',
+  'At least I\'m not a teapot.',
+  'Ravioli, ravioli, that page doesn\'t exist-ioli.',
+  'This page is trying its best.',
+  'Roses are red. Violets are blue. I wish I could\'ve found that page for you.'
+]
+
+messageElem.innerHTML = messages[RNGesus(0, messages.length - 1)];
+
+function RNGesus(min, max) {
+  let num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
